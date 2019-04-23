78d1efaa3f4bde54bc209d5f905f39dfeaf420a5
Moisan Simon <simon.moisan@etudiant.univ-rennes1.fr>
2019-04-23 09:38:25 +0200

    Ajout d'une API fonctionnelle
     Please enter the commit message for your changes. Lines starting
    	node_modules/

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const helmet = require('helmet')
 const usersRouter = require('./routes/users-v1')
 const usersModel = require('./model/users')
 
+const useLoggerRouter = require("./routes/auth-v1");
+const loggerModel = require("./model/idp");
+const logger = loggerModel(usersModel);
+
 const app = express()
 
 app.use(bodyParser.json())
@@ -15,6 +19,6 @@ app.use(helmet({noSniff: true}))
 // On injecte le model dans les routers. Ceci permet de supprimer la dépendance
 // directe entre les routers et le modele
 app.use('/v1/users', usersRouter(usersModel))
-
+app.use("/v1/auth", useLoggerRouter(usersModel, logger));
 // For unit tests
 exports.app = app
--- model/users.js
+++ model/users.js
@@ -1,10 +1,13 @@
 const uuidv1 = require('uuid/v1')
 const tcomb = require('tcomb')
+const bcrypt = require("bcrypt");
+const salt_round = 10;
 
 const USER = tcomb.struct({
     id: tcomb.String,
     name: tcomb.String,
     login: tcomb.String,
+    password: tcomb.String,
     age: tcomb.Number
 }, {strict: true})
 
@@ -13,21 +16,25 @@ const users = [
         id: '45745c60-7b1a-11e8-9c9c-2d42b21b1a3e',
         name: 'Pedro Ramirez',
         login: 'pedro',
+        password: bcrypt.hashSync("caca", salt_round),
         age: 44
     }, {
         id: '456897d-98a8-78d8-4565-2d42b21b1a3e',
         name: 'Jesse Jones',
         login: 'jesse',
+        password: bcrypt.hashSync("caca", salt_round),
         age: 48
     }, {
         id: '987sd88a-45q6-78d8-4565-2d42b21b1a3e',
         name: 'Rose Doolan',
         login: 'rose',
+        password: bcrypt.hashSync("caca", salt_round),
         age: 36
     }, {
         id: '654de540-877a-65e5-4565-2d42b21b1a3e',
         name: 'Sid Ketchum',
         login: 'sid',
+        password: bcrypt.hashSync("caca", salt_round),
         age: 56
     }
 ]
